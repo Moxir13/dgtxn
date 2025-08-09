ff7561fb8b9c6f304312a6fc8dc681aa9c03e2da
Moxir13 <r9294982@gmail.com>
2025-08-09 10:02:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let randomSon=Math.floor(Math.random()*90+10);
+alert("Sizda hozir random son chiqishni boshlaydi: ");
+alert("random orqali chiqqan son:" +randomSon);
+confirm("Biz buni 2 ga ko'paytirib bo'lmoqchimiz.");
